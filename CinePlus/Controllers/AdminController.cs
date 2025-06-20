14215514aff0f14dacb8e213896fe7183f11fac7
Dara SatishKumar <dara.satishkumar@mphasis.com>
2025-06-20 19:20:32 +0530

    second part

--- CinePlus/Controllers/AdminController.cs
+++ CinePlus/Controllers/AdminController.cs
@@ -2,11 +2,52 @@
 
 namespace CinePlus.Controllers
 {
+    [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]    
     public class AdminController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
-    }
+        [HttpGet]
+        public IActionResult Logout()
+        {
+
+
+            HttpContext.Session.Clear();   
+            return RedirectToAction("Login", "Admin");
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+           
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(IFormCollection f)
+        {
+            if (f["uname"]=="admin"  && f["pwd"] == "123")
+            {
+                HttpContext.Session.SetString("uname", f["uname"]);
+                return RedirectToAction("Home", "Admin");
+            }
+            else
+            {
+                ViewBag.Error = "Invalid username or password";
+                return View();
+            }
+
+         
+        }
+        [HttpGet]
+        public IActionResult Home()
+        {
+            if (HttpContext.Session.GetString("uname") == null)
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+            return View();
+      
+        }  }
 }
--- CinePlus/obj/Debug/net8.0/CinePlus.AssemblyInfo.cs
+++ CinePlus/obj/Debug/net8.0/CinePlus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinePlus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f8fbea769f81e144f617d5965a9e70f4f9e30b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinePlus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinePlus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
