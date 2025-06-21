7b0f3892ad225f10075a314a6c9d953aced7232a
booa2002 <kumarprince14015@gmail.com>
2025-06-21 21:04:21 +0530

    Added the MovieList

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,13 +1,99 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CinePlus.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CinePlus.Controllers
 {
     public class UserController : Controller
     {
+        CinePlusContext pc = new CinePlusContext();
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        //This for listing all the movies
+        [HttpGet]
+        public IActionResult Movies()
+        {
+            var movie = pc.Movies.ToList();
+            ViewBag.genre = pc.Genres.ToList();
+            ViewBag.language = pc.Languages.ToList();
+            return View(movie);
+        }
+        //This is for Filter the Movie
+        [HttpPost]
+        public IActionResult Movies(IFormCollection f)
+        {
+            if (f["option"] == "All" && f["Lang"] == "All")
+            {
+                return RedirectToAction("Movies");
+            }
+            else if (f["option"] != "All" && f["Lang"] != "All")
+            {
+                var searchTerm = Convert.ToInt32(f["Option"]);
+                var langsearch = Convert.ToInt32(f["Lang"]);
+                ViewBag.genre = pc.Genres.ToList();
+                ViewBag.language = pc.Languages.ToList();
+
+                // LINQ: Filter movies by both GenreId and LanguageId
+                var movies = (from m in pc.Movies
+                              join ml in pc.MovieLanguages on m.MovieId equals ml.MovieId
+                              where m.GenreId == searchTerm && ml.LanguageId == langsearch
+                              select m).Distinct().ToList();
+
+                return View(movies);
+            }
+            else if (f["option"] != "All")
+            {
+                var searchTerm = Convert.ToInt32(f["Option"]);
+                ViewBag.t = searchTerm;
+                ViewBag.genre = pc.Genres.ToList();
+                ViewBag.language = pc.Languages.ToList();
+                var movies = pc.Movies.Where(t => t.GenreId == searchTerm).ToList();
+                return View(movies);
+            }
+            else if (f["Lang"] != "All")
+            {
+                var langsearch = Convert.ToInt32(f["Lang"]);
+                ViewBag.genre = pc.Genres.ToList();
+                ViewBag.language = pc.Languages.ToList();
+                var movieIds = pc.MovieLanguages
+                    .Where(t => t.LanguageId == langsearch)
+                    .Select(t => t.MovieId)
+                    .ToList();
+                var movies = pc.Movies.Where(m => movieIds.Contains(m.MovieId)).ToList();
+                return View(movies);
+            }
+            else
+            {
+                return RedirectToAction("Movies");
+            }
+        }
+        [HttpGet]
+        //id=@movie.MovieId&name=@movie.MovieName&duration=@movie.Duration&desc=@movie.Description&release=@movie.ReleaseDate&image
+        public IActionResult MovieDetail(string id,string name,string duration,string desc,string release)
+        {
+            ViewBag.id = id;
+            ViewBag.name = name;
+            ViewBag.duration = duration;
+            ViewBag.desc = desc;
+            ViewBag.release = release;
+            return View();
+        }
         public IActionResult LogOut()
         {
             HttpContext.Session.Clear();
             return RedirectToAction("Index", "CinePlus");
         }
+
     }
 }
--- obj/Debug/net8.0/CinePlus.AssemblyInfo.cs
+++ obj/Debug/net8.0/CinePlus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinePlus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98138ed9fa586d962373ac4cb81ddb43d7831678")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e986787e0b1d3ad18b160192f09f82601f6bda32")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinePlus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinePlus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
