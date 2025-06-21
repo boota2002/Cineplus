e986787e0b1d3ad18b160192f09f82601f6bda32
booa2002 <kumarprince14015@gmail.com>
2025-06-21 09:01:39 +0530

    User Layout

---
+++ Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CinePlus.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult LogOut()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "CinePlus");
+        }
+    }
+}
